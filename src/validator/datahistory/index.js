8c76c42d84c1e677255e4c04d9416df50ce026f9
xharf <123190123@student.upnyk.ac.id>
2021-11-21 14:56:00 +0700

    change folder name

---
+++ src/validator/datahistory/index.js
@@ -0,0 +1,15 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { DataHistoryPlayloadSchema } = require('./schema');
+
+const DataHistoryValidator = {
+  validateDataHistoryPayload: (payload) => {
+      const validationResult = DataHistoryPlayloadSchema.validate(payload);
+
+      if (validationResult.error) {
+          throw new InvariantError(validationResult.error.message);
+      }
+  },
+};
+
+
+module.exports = DataHistoryValidator;
